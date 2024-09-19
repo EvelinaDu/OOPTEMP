5e6ea9c5d8d6ec6e48f7915488e534486d969b42
EvelinaDu <evelinadudutyte9@gmail.com>
2024-09-19 23:23:57 +0300

    Įtrauktos naudojamos bibliotekos, sukurta studento struktūra.

---
+++ namu_darbai.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+struct Studentas{
+  string vardas, pavarde;
+  vector <double> nd;
+  double egz, galutinis_vid, galutinis_med;
+};
+
+int main() {
+    vector<Studentas> stud;
+    Studentas s;
+    
+}
