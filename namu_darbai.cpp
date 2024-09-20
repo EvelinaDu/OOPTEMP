20e287414bac8ea18a6241205bac21cea44978e0
EvelinaDu <evelinadudutyte9@gmail.com>
2024-09-20 23:05:33 +0300

    Sukurta duomenų generavimo void funkcija, vardą bei pavardę reikia įvesti vartotojui, tačiau įvertinimai už namų darbus ir egzaminą yra sugeneruojami dešimtbalėje vertinimo sistemoje.

--- namu_darbai.cpp
+++ namu_darbai.cpp
@@ -4,6 +4,10 @@
 #include <iomanip>
 using namespace std;
 
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
+
+
 struct Studentas{
   string vardas, pavarde;
   vector <double> nd;
@@ -52,6 +56,34 @@ void Duom_ivedimas(Studentas &s){
 
         cout << "Įveskite studento egzamino įvertinimą: ";
         cin >> s.egz;
+}
+
+void Duom_generavimas(Studentas &s){
+    cout << "Įveskite studento vardą: ";
+    cin >> s.vardas;
+
+    cout << "Įveskite studento pavarde: ";
+    cin >> s.pavarde;
+
+    int nd_kiekis;
+    cout << "Kiek namų darbų norėtumėt, kad būtų sugeneruota? ";
+    cin >> nd_kiekis;
+
+    int nd_nr = 1;
+    srand(time(0));
+
+    for(int i = 0; i < nd_kiekis; i++){
+
+        int nd_ivertinimas = rand() % 10 + 1;
+
+        cout << nd_nr <<" Namų darbų" << " įvertinimas: " << nd_ivertinimas << endl;
+        s.nd.push_back(nd_ivertinimas);
+        nd_nr ++;
+
+    }
+
+    s.egz = rand() % 10 + 1;
+    cout << "Egzamino įvertinimas: " << s.egz << endl;
 }
 
 int main() {
