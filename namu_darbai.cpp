d329e336c4d0937f2805f0cd14f29086bf2606f9
EvelinaDu <evelinadudutyte9@gmail.com>
2024-09-20 19:03:58 +0300

    Sukurta duomenų įvedimo void funkcija, kuri leidžia vartotojui įvesti studento vardą, pavardę, namų darbų įvertinimus ir egzamino įvertinimą.

--- namu_darbai.cpp
+++ namu_darbai.cpp
@@ -10,8 +10,53 @@ struct Studentas{
   double egz, galutinis_vid, galutinis_med;
 };
 
+
+void Duom_ivedimas(Studentas &s){
+        string pasirinkimas;
+        bool ivertinimo_ivedimas = false;
+
+
+        cout << "Įveskite studento vardą: ";
+        cin >> s.vardas;
+
+        cout << "Įveskite studento pavarde: ";
+        cin >> s.pavarde;
+
+
+        cout << "Ar norite įvesti namų darbų įvertinimą? ";
+        cin >> pasirinkimas;
+
+        if (pasirinkimas == "Taip" || pasirinkimas == "taip"){
+            ivertinimo_ivedimas = true;
+        } else {
+            ivertinimo_ivedimas = false;
+        }
+
+        int ivertinimas;
+        int nd_nr = 1;
+        while(ivertinimo_ivedimas){
+            cout << nd_nr <<" Namų darbų" << " įvertinimas: ";
+            nd_nr ++;
+            cin >> ivertinimas;
+            s.nd.push_back(ivertinimas);
+
+            cout << "Ar norite įvesti namų darbų įvertinimą? ";
+            cin >> pasirinkimas;
+
+            if (pasirinkimas == "Taip" || pasirinkimas == "taip"){
+                ivertinimo_ivedimas = true;
+            } else {
+                ivertinimo_ivedimas = false;
+            }
+        }
+
+        cout << "Įveskite studento egzamino įvertinimą: ";
+        cin >> s.egz;
+}
+
 int main() {
     vector<Studentas> stud;
     Studentas s;
-    
+
+
 }
